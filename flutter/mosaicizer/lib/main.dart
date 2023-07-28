26b41bbcbcd602103f86f0ea9116129cc7864e47
Danny Kim <imbird0312@gmail.com>
2023-07-29 04:15:09 +0900

    update web (#1)

--- flutter/mosaicizer/lib/main.dart
+++ flutter/mosaicizer/lib/main.dart
@@ -1,57 +1,102 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
-
-// don't forget this line
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Remove the debug banner
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.amber),
-      title: "Kindacode.comr",
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
-  // Create a webview controller
-  final _controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..setNavigationDelegate(
-      NavigationDelegate(
-        onPageStarted: (String url) {},
-        onPageFinished: (String url) {},
-        onWebResourceError: (WebResourceError error) {},
-        onNavigationRequest: (NavigationRequest request) {
-          return NavigationDecision.navigate;
-        },
-      ),
-    )
-    ..loadRequest(Uri.parse("http://localhost:8000/integrated.html"));
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  late InAppWebViewController _controller;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Webview Example'),
+      body: InAppWebView(
+        initialUrlRequest: URLRequest(url: Uri.parse('https://dh031200.github.io/mosaicizer/')),
+        onWebViewCreated: (InAppWebViewController controller) {
+          _controller = controller;
+        },
+        onProgressChanged: (InAppWebViewController controller, int progress) {
+          if (progress == 100) {
+            _controller.addJavaScriptHandler(handlerName: 'fileChooser', callback: (args) {
+              _getImage();
+            });
+            _controller.addJavaScriptHandler(handlerName: 'saveImage', callback: (args) {
+              _saveImage(args.first);
+            });
+            _controller.evaluateJavascript(source: '''
+              var saveBtn = document.querySelector('#saveBtn');
+              saveBtn.addEventListener('click', function () {
+                const resultCanvas = document.getElementById('result');
+                const dataUrl = resultCanvas.toDataURL();
+                window.flutter_inappwebview.callHandler('saveImage', dataUrl);
+              });
+            ''');
+          }
+        },
+        onLoadResource: (InAppWebViewController controller, LoadedResource resource) {
+          _controller.evaluateJavascript(source: '''
+            var meta = document.createElement('meta');
+            meta.setAttribute('name', 'viewport');
+            meta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0');
+            document.getElementsByTagName('head')[0].appendChild(meta);
+          ''');
+        },
       ),
-      body: SizedBox(
-          width: double.infinity,
-          // the most important part of this example
-          child: WebViewWidget(
-            controller: _controller,
-          )),
     );
+  }
+
+  void _getImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? imageFile = await picker.pickImage(source: ImageSource.gallery);
+    if (imageFile != null) {
+      final Uint8List bytes = await imageFile.readAsBytes();
+      final String base64 = base64Encode(bytes);
+      _controller.evaluateJavascript(source: 'window.uploadFile("$base64")');
+    }
+  }
+
+  void _saveImage(String dataUrl) async {
+    final RegExp regex = RegExp(r'data:image/png;base64,(.+)');
+    final Match? match = regex.firstMatch(dataUrl);
+    if (match != null && match.groupCount == 1) {
+      final String base64 = match.group(1)!;
+      final Uint8List bytes = base64Decode(base64);
+      final String tempDir = (await getTemporaryDirectory()).path;
+      final String filePath = '$tempDir/result.png';
+      final File file = File(filePath);
+      await file.writeAsBytes(bytes);
+      final bool? result = await GallerySaver.saveImage(filePath, albumName: 'Download');
+      print('Image saved to gallery: $result');
+      if (result == true) {
+        Fluttertoast.showToast(msg: "Image saved successfully!");
+      }
+    }
   }
 }
